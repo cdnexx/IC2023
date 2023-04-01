87eb052ffe39ed37df2364384b94c97a54d0d6e1
cdnexx <franco.oc99@gmail.com>
2023-04-01 19:36:38 -0300

    Mode selection, message return and date filename working

--- Rigol/main.py
+++ Rigol/main.py
@@ -1,26 +1,14 @@
+"""Main module"""
+import json
 from PyQt5 import QtWidgets
 from rigol_ui2 import Ui_MainWindow
-import json
 
-
-def read_config():
-    with open("configs/last_config.json") as config_file:
-        config = json.load(config_file)
-    func = config["config"]["type"]
-    freq = config["config"]["frequency"]
-    ampl = config["config"]["amplitude"]
-    return func, freq, ampl
-
-
-config = read_config()
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow(initial_type=config[0],
-                       initial_freq=config[1],
-                       initial_ampl=config[2])
+    ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
