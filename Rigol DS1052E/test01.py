8c27562b21f298a151b13bae638c9ead4c9b3092
Franco <franco.oc99@gmail.com>
2023-05-02 16:33:19 -0400

    Update

--- Rigol DS1052E/test01.py
+++ Rigol DS1052E/test01.py
@@ -2,23 +2,31 @@ import pyvisa
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Connect to the instrument
+# Connect to the oscilloscope
 rm = pyvisa.ResourceManager()
-scope = rm.open_resource('USB0::0x1AB1::0x0588::DS1ET200601265::INSTR')
+scope = rm.open_resource('USB0::0x1AB1::0x0588::DS1ED141904883')
 
-# Set up the waveform acquisition parameters
-scope.write(':WAV:SOUR CHAN1')  # select channel 1
-scope.write(':WAV:MODE NORM')  # set normal mode
-scope.write(':WAV:FORM ASC')   # set ASCII waveform format
-scope.write(':WAV:POIN:MODE RAW')  # set raw point mode
-scope.write(':WAV:POIN 1000')  # set number of points to acquire
+# Set up the oscilloscope for data acquisition
+scope.write(':STOP')
+scope.write(':WAV:SOUR CHAN1')
+scope.write(':WAV:MODE NORM')
+scope.write(':WAV:FORM ASCII')
+scope.write(':WAV:POINTS:MODE RAW')
+scope.write(':WAV:POINTS 1000')
 
-# Acquire the waveform data
-data_ascii = scope.query(':WAV:DATA?')
-data = np.fromstring(data_ascii, sep=',')  # convert ASCII data to numpy array
+# Acquire data from the oscilloscope
+data_str = scope.query(':WAV:DATA?')
 
-# Plot the waveform data
-plt.plot(data)
-plt.xlabel('Time (samples)')
+# Convert the data from string format to a numpy array
+data = np.array(data_str.strip().split(','), dtype=float)
+
+# Calculate the time array
+timestep = float(scope.query(':WAV:XINC?'))
+time = np.arange(len(data)) * timestep
+
+# Plot the data
+plt.plot(time, data)
+plt.xlabel('Time (s)')
 plt.ylabel('Voltage (V)')
+plt.title('Oscilloscope data')
 plt.show()
