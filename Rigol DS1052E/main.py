74bc3503430927bddee86d06d90080fa8b2e9068
Franco <franco.oc99@gmail.com>
2023-05-16 17:36:46 -0400

    Display both channels

--- Rigol DS1052E/main.py
+++ Rigol DS1052E/main.py
@@ -30,37 +30,49 @@ class GraphCanvas(FigureCanvas):
                                          timeout=20, chunk_size=1024000)
         self.plot_data()
 
+    def get_channel_data(self, channel: int):
+        # Get volt scale
+        voltscale = float(self.osc.query(f':CHAN{channel}:SCAL?')[0])
+
+        # Get the voltage offset
+        voltoffset = float(self.osc.query(f":CHAN{channel}:OFFS?")[0])
+
+        self.osc.write(f":WAV:DATA? CHAN{channel}")  # Request the data
+        raw_data = self.osc.read_raw()  # Read the data
+        raw_data = raw_data[10:]  # Remove header (first 10 characters)
+
+        data = np.frombuffer(raw_data, 'B')
+        data = data * -1 + 255
+        data = (data - 130.0 - voltoffset/voltscale*25) / 25 * voltscale
+
+        return data
+
     def plot_data(self):
+
         # Grab raw data from CH1
-        self.osc.write(":STOP")
+        # self.osc.write(":STOP")
 
         # Get the timescale
         timescale = float(self.osc.query(":TIM:SCAL?"))
 
         # Get the timescale offset
         timeoffset = float(self.osc.query(":TIM:OFFS?")[0])
-        voltscale = float(self.osc.query(':CHAN1:SCAL?')[0])
-
-        # Get the voltage offset
-        voltoffset = float(self.osc.query(":CHAN1:OFFS?")[0])
 
         self.osc.write(":WAV:POIN:MODE RAW")
-        self.osc.write(":WAV:DATA? CHAN1")  # Request the data
-        raw_data = self.osc.read_raw()  # Read the data
-        raw_data = raw_data[10:]  # Remove header (first 10 characters)
-        data_size = len(raw_data)
+
+        # data_size = len(raw_data)
         # sample_rate = float(self.osc.query(':ACQ:SAMP?')[0]) # Get the sample rate
 
         self.osc.write(":KEY:FORCE")
-        self.osc.write(":RUN")
+        # self.osc.write(":RUN")
 
-        data = np.frombuffer(raw_data, 'B')
-        data = data * -1 + 255
-        data = (data - 130.0 - voltoffset/voltscale*25) / 25 * voltscale
+        # Get data from each channel
+        data_ch1 = self.get_channel_data(channel=1)
+        data_ch2 = self.get_channel_data(channel=2)
 
         time = np.linspace(timeoffset - 6 * timescale,
                            timeoffset + 6 * timescale,
-                           num=len(data))
+                           num=len(data_ch1))
 
         if (time[-1] < 1e-3):
             time = [t * 1e6 for t in time]
@@ -71,9 +83,12 @@ class GraphCanvas(FigureCanvas):
         else:
             tUnit = "S"
 
-        plt.plot(time, data)
-        plt.title('Osciloscopio CH1')
-        plt.ylabel('Volateje (V)')
+        # Plot each channel
+        ax = plt.subplot()
+        ax.plot(time, data_ch1)
+        ax.plot(time, data_ch2)
+
+        plt.ylabel('Voltaje (V)')
         plt.xlabel("Tiempo (" + tUnit + ")")
         plt.xlim(time[0], time[-1])
 
